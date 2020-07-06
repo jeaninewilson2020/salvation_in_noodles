ca943793b666a47450e8eb0ee523fcc6e969fdc9
Jeanine <62892692+jeaninewilson2020@users.noreply.github.com>
2020-07-06 10:46:53 +0100

    Add files via upload
    validateForm function mostly working as it should, add required attribute to the form in html

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
+const { clear } = require("console");
 
-
-function isEmpty(str) {
-    "use strict";
-    return (str.length === 0 || !str.trim());
-}
 
 function validatePhone(inputtext) {
     "use strict";
@@ -25,15 +21,8 @@ function formatPhoneNumber(phoneNo) {
            + phoneNo.substring(7)
  }
 
-function validateEmail(email) {
-    "use strict";
-    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,15}(?:\.[a-z]{2})?)$/i;
-    return isEmpty(email) || re.test(email);
-}
-
 
 function validateDate(enteredDate) {
-    "use strict";
     var idate = document.getElementById("date"), 
     dateReg = /(0[1-9]|[12][0-9]|3[01])[\/](0[1-9]|1[012])[\/]201[4-9]|20[2-9][0-9]/;
     if (!dateReg.test(idate.value)) {
@@ -44,12 +33,12 @@ function validateDate(enteredDate) {
     
 }
 
-/*
+// Works in 24hr format, but someone could enter 12pm and it would still go through 
+
 function validateTime(enteredTime) {
     re = /^(\d{1,2}):(\d{2})([ap]m)?$/;
     if (document.getElementById('time').value != '') {
         if (regs = document.getElementById('time').value.match(re)) {
-
             if (regs[3]) {
              // 12-hour value between 6 and 10 
                 if (regs[1] < 6 || regs[1] > 10) {
@@ -73,43 +62,30 @@ function validateTime(enteredTime) {
         }
     }
 }
-*/
+
 
 function validateForm() {
-    if (isEmpty(document.getElementById('name').value.trim())) {
-        alert('Name is required!');
-        return false;
+
+     if (!validateDate(document.getElementById('date'))) {
+        alert("Please enter a valid date");
+        return false; 
     }
+
     else if (!validatePhone(document.getElementById('phone'))) {
         alert("Please enter a valid 11 digit UK phone number");
         return false;
-    } else {
+    } 
+    
+
+    else if (!validateTime(document.getElementById('time'))) {
+        return false; 
+    }
+    
+    else {
         (formatPhoneNumber(document.getElementById('phone'))); 
     }
-    if (!validateEmail(document.getElementById('email').value.trim())) {
-        alert('Email must be a valid email address!');
-        return false;
-    }
-    else if(isEmpty(document.getElementById('email').value.trim())) {
-        alert('Email is required!');
-        return false;
-    }
-    else if (isEmpty(document.getElementById('date').value.trim())) {
-        alert('Date is required!');
-        return false;
-    } else if (!validateDate(document.getElementById('date'))) {
-        alert("Please enter a valid date");
-    }
-    else if (isEmpty(document.getElementById('time').value.trim())) {
-        alert('Time is required!');
-        return false; /*
-    } else if (!validateTime(document.getElementById('time'))) {
-        alert("Please enter a valid time. We are open for bookings Monday to Sunday, 6pm to 10pm.")
-    }
-  */
-  }
+
+
 }
 
    
-
-    
